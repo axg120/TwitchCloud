8a5647616dae701ccab56068e4c0ce605846d1d4
andyg <axg5351@rit.edu>
2018-06-22 16:40:39 -0400

    dependencies

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const tmi = require('tmi.js');
+var express = require("express");
+var socket = require("socket.io");
+var tmi = require("tmi.js");
 
 var opts = {
   identity: {
-    username: 'twitchcloudbot',
-    password: 'oauth:5qbcbkk0hexlw3hdo14a8q0hqi04lt'
+    username: "twitchcloudbot",
+    password: "oauth:5qbcbkk0hexlw3hdo14a8q0hqi04lt"
   },
-  channels: ['ninja']
+  channels: ["ninja"]
 };
 
 // Create a client with our options:
 var client = new tmi.client(opts);
 client.connect();
 
-client.on('chat', function(channel, user, message, self) {
+client.on("chat", function(channel, user, message, self) {
   console.log(message);
 });
