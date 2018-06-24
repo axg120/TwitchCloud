f6f5c83ce0d4f75bcc5d941ef3e499471c517a0e
andyg <axg5351@rit.edu>
2018-06-24 13:53:56 -0400

    cloud color and orientation

--- public/embed.js
+++ public/embed.js
@@ -2,9 +2,9 @@ function embedTwitch(channel="twitch") {
   var vh = window.innerHeight;
   var vw = window.innerWidth;
 
-  height = vh * .5;
-  width = vw * .5;
-  var embed = new Twitch.Embed("twitch-embed", {
+  var height = vh * .5;
+  var width = vw * .5;
+  new Twitch.Embed("twitch-embed", {
     width: width,
     height: height,
     channel: channel,
--- public/script.js
+++ public/script.js
@@ -23,25 +23,50 @@ $("#channelBtn").click(function() {
   }
 });
 
-var layout = d3.layout.cloud().size([1000, 500]);
+var vh = window.innerHeight;
+var vw = window.innerWidth;
+
+var height = vh * .5;
+var width = vw * .5;
+
+var layout = d3.layout.cloud().size([width, height]);
+var color = d3.scaleOrdinal(d3.schemeSet3);
 
 function draw(words) {
+  if(words.length >= 100) words = words.splice(0, 50);
   $("#wordcloud").empty();
-  d3.select("#wordcloud").append("svg")
+  var cloud = d3.select("#wordcloud").append("svg")
       .attr("width", layout.size()[0])
       .attr("height", layout.size()[1])
     .append("g")
       .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
     .selectAll("text")
-      .data(words)
-    .enter().append("text")
-      .style("font-size", function(d) { return d.size + "px"; })
-      .style("font-family", "Impact")
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) {
+      .data(words);
+
+  cloud.enter().append("text")
+    .style("font-size", function(d) { return d.size + "px"; })
+    .style("font-family", "Impact")
+    .attr("fill",function(d,i){return color(i);})
+    .attr("text-anchor", "middle")
+    .attr("transform", function(d) {
+      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+    })
+    .text(function(d) { return d.text; });
+
+  cloud.transition()
+    .duration(600)
+    .style("font-size", function(d) { return d.size + "px"; })
+    .attr("transform", function(d) {
         return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text(function(d) { return d.text; });
+    })
+    .style("fill-opacity", 1);
+
+  cloud.exit()
+    .transition()
+    .duration(200)
+    .style('fill-opacity', 1e-6)
+    .attr('font-size', 1)
+    .remove();
 }
 
 setInterval(function() {
@@ -49,7 +74,7 @@ setInterval(function() {
       return {text: d, size: 10 + Math.random() * 90, test: "haha"};
     }))
     .padding(5)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .rotate(function() { return ~~(Math.random() * 2); })
     .font("Impact")
     .fontSize(function(d) { return d.size; })
     .on("end", draw)
